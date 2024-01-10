81fa4fec68896278e8fe48ad0d8710298e1b4790
Dominika Ducka <dominika.ducka2@gmail.com>
2024-01-10 20:23:12 +0100

    database and insert user data

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -1,27 +1,42 @@
 <?php
 
-//operators
  // var_dump($_SERVER["REQUEST_METHOD"]);
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
-  $firstname = htmlspecialchars( $_POST["firstname"]);
-  $lastname = htmlspecialchars( $_POST["lastname"]);
-  $pets = htmlspecialchars( $_POST["favpet"]);
+  $firstname = $_POST["firstname"];
+  $lastname = $_POST["lastname"];
+  $pet = $_POST["favpet"];
+  $pet_name = $_POST["petname"];
 
   if (empty($firstname)) {
+    header("Location: ../index.php?success=false");
     exit();
-    header("Location: ../index.php");
   }
 
-  echo "These are the data, that user submitted: ";
-  echo"<br>";
-  echo $firstname;
-  echo "<br>";
-  echo $lastname;
-  echo "<br>";
-  echo $pets;
+  try {
+      require_once "dbh.inc.php";
 
-  header("Location: ../index.php");
+      $query = "INSERT INTO users (firstname, lastname, pet_id) VALUES
+      (:firstname, :lastname, :pet_id);";
+
+      $stmt = $pdo->prepare($query);
+
+      $stmt->bindParam(":firstname", $firstname);
+      $stmt->bindParam(":lastname", $lastname);
+      $stmt->bindParam(":pet_id", $pet_id);
+
+      $stmt->execute([]);
+      
+      $pdo = null;
+      $stmt = null;
+      
+      header("Location: ../index.php?success=true");
+      die();
+  } catch (PDOException $e) {
+      die("Query failed: " . $e->getMessage());
+  };
+
+  header("Location: ../index.php?success=true");
  } else {
-  header("Location: ../index.php")
- }
+    header("Location: ../index.php");
+ };
